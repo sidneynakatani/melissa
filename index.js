a6f2c985401117ac5aa9da925371f796ce25f83f
sidney <sidney.nakatani@hotmail.com>
2016-04-17 21:03:41 -0300

    alteracoes

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
-var fs = require('fs');
-var nosql = require('nosql').load(__dirname +'/database.nosql');
 
-//nosql.insert({ name: 'Peter' });
-var x = nosql.views.all('young', function(err, documents, count) {
-	// view file not created 
-	// documents === empty 
-}, 0, 10);
+mongoose.connect('mongodb://admin:zarman12@ds059205.mongolab.com:59205/ragdoll');
 
-console.log(x);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log(' connected!');
+});
+
+var testeSchema = new mongoose.Schema({teste: String});
+var Teste = mongoose.model('Teste',testeSchema);
+var testando = new Teste({
+  teste: 'teste'
+});
+
+testando.save(function(err, testando){
+    if(err){
+        return console.error(err);
+    }
+    console.log('gravado');
+    
+});
 
 app.set('port', (process.env.PORT || 5000));
-
-app.get('/listUsers', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       res.end( data );
-   });
-})
-
-app.get('/:id', function (req, res) {
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       users = JSON.parse( data );
-       var user = users["user" + req.params.id] 
-       res.end( JSON.stringify(user));
-   });
-})
 
 
 app.listen(app.get('port'), function() {
